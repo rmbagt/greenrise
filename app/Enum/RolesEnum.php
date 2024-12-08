038dd16d6eb460d50c8a3f561d4e3696bc5ae25f
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-12-08 17:43:37 +0700

    feat: smtp email verification

--- app/Enum/RolesEnum.php
+++ app/Enum/RolesEnum.php
@@ -6,4 +6,21 @@ enum RolesEnum: string
 {
     case Admin = 'admin';
     case User = 'user';
+
+    public static function labels(): array
+    {
+        return [
+            self::Admin->value => 'Admin',
+            self::User->value => 'User',
+        ];
+    }
+
+
+    public function label()
+    {
+        return match($this) {
+            self::Admin => 'Admin',
+            self::User => 'User',
+        };
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Enum\PermissionsEnum;
+use App\Enum\RolesEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $userRole = Role::create(['name' => RolesEnum::User->value]);
+        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+
+        $manageEventsPermission = Permission::create(['name' => PermissionsEnum::ManageEvents->value]);
+        $manageUsersPermission = Permission::create(['name' => PermissionsEnum::ManageUsers->value]);
+        $donatesPermission = Permission::create(['name' => PermissionsEnum::Donates->value]);
+
+        $userRole->syncPermissions([$donatesPermission]);
+        $adminRole->syncPermissions([$manageEventsPermission, $manageUsersPermission, $donatesPermission]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'name' => 'User User',
+            'email' => 'user@example.com',
+        ])->assignRole(RolesEnum::User);
+
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+        ])->assignRole(RolesEnum::Admin);
     }
 }
